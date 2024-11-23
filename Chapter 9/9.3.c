27495229ceada07aac0f20634e4d062ecb60f54e
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-24 01:34:14 +0500

    9.3

---
+++ Chapter 9/9.3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+	int a,b;
+	printf("Enter First Number\n");
+	scanf("%d",&a);
+	printf("Enter Second Number\n");
+	scanf("%d",&b);
+	printf("Addition=%d\n",a+b);
+    printf("Subtration is=%d\n",a-b);
+    printf("Multiplication is=%d\n",a*b);
+    printf("Division is=%d\n",a/b);
+    printf("Modolus is=%d\n",a%b);
+	getch();
+}
