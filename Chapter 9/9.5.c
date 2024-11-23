dedf871f5a79e83a73fdc66fa0c1ec6a52aa4622
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-24 01:34:30 +0500

    9.5

---
+++ Chapter 9/9.5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+	int a,b,c,d;
+	a=10;//here we can use any value
+	b=10;
+	c=++a;
+	d=b++;
+	printf("Value Of a=%d\n",a);
+    printf("Value Of c=%d\n",c);
+	printf("Value Of b=%d\n",b);
+	printf("Value Of d=%d\n",d);
+	getch();
+}
