44414fdf57e7e480388de71bf6bffffe4ab56ceb
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-24 01:32:40 +0500

    chapter 9 solution c files

---
+++ Chapter 9/9.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	short a=32767;
+	printf("%d\n",a);
+	  a=a+1;
+    printf("%d\n",a);
+     a=a-1;
+	printf("%d\n",a);
+	 getch();    
+    
+	
+	
+}
