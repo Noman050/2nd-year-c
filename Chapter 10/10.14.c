13e9971c1addfe5c52ca0b30367b0fe307e935bd
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-25 01:58:29 +0500

    10.14

---
+++ Chapter 10/10.14.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	float b,h,a;
+	printf("Enter The Base\n");
+	scanf("%f",&b);
+	printf("Enter The Height\n");
+	scanf("%f",&h);
+	a=0.5*b*h;
+	printf("Area Is=%f",a);
+	getch();
+	
+}
