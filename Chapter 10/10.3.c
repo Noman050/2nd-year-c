b037935cbb0bd015132d923604161e36969dbbc0
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-25 01:56:58 +0500

    10.3

---
+++ Chapter 10/10.3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int h,w,a;
+	printf("Enter The Height Of The Square\n");
+	scanf("%d",&h);
+	printf("Enter The Width Of The Square\n");
+	scanf("%d",&w);
+	a=h*w;
+	printf("Area Of Squre Is=%d\n",a);
+	getch();
+}
