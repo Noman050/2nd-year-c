e1aa083d7ee1ca17101086eafdf3717590363b68
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-25 01:56:52 +0500

    10.2

---
+++ Chapter 10/10.2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	float a,b,c;
+	printf("Enter The First Float Value\n");
+	scanf("%f",&a);
+	printf("Enter The Second Float Value\n");
+	scanf("%f",&b);
+	c=a+b;
+	printf("Sum=%f\n",c);
+	getch();
+}
