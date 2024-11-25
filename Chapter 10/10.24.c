339e157cf7f9de858f1e897fb1a2d2c5dd119d49
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-26 01:47:54 +0500

    10.24

---
+++ Chapter 10/10.24.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	{
+	do
+	char ch;
+	printf("Enter Character\n");
+	scanf("%c",&ch);
+	printf("\n The ASCII Code of %c is %d",ch,ch);
+	getch();
+	while(n!=-0);
+}
