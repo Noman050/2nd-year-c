c58db9ec5f79a575a162161ca678a10e72875f06
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-27 23:26:34 +0500

    11.4

---
+++ Chapter 11/11.4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n;//most imp numerical ever
+	printf("Enter a number\n");
+	scanf("%d",&n);
+	if(n%2==0)
+	printf("It is even=%d",n);
+	else
+	printf("It is odd=%d",n);
+	getch();
+}
