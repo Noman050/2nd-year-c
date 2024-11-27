1aa4a587421396afedea6da087317664d6105f4a
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-27 23:26:40 +0500

    11.5

---
+++ Chapter 11/11.5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b;
+	printf("Enter The First Number\n");
+	scanf("%d",&a);
+	printf("Enter The 2nd Number\n");
+	scanf("%d",&b);
+	if(a==b)
+	printf("These Numbers are Equal\n");
+	else
+	printf("These Numbers are not Equal\n");
+	getch();
+}
