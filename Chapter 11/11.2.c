57346f5b9bbf56fd539dac762109cc35e499aea9
Noman Ali <107186063+Noman050@users.noreply.github.com>
2024-11-27 23:26:21 +0500

    11.2

---
+++ Chapter 11/11.2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b;
+	printf("Enter First number\n");
+	scanf("%d",&a);
+	printf("Enter 2nd Number\n");
+	scanf("%d",&b);
+	if(a*a==b)
+	printf("2nd Number is Squre of 1st one\n");
+	getch();
+}
